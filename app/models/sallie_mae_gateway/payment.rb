07f30556137c06d0c05cc13bc27f12049ef8dfa9
Daniel Reedy <danreedy@gmail.com>
2012-03-21 14:49:54 -0500

    Correcting payment form to use provided postback urls

--- app/models/sallie_mae_gateway/payment.rb
+++ app/models/sallie_mae_gateway/payment.rb
@@ -5,7 +5,7 @@ module SallieMaeGateway
     attr_accessor :student, :line_items
     attr_accessor :school_id, :postback_url, :finish_url, :cancel_url, :user_def1, :user_def2, :allow_edit, :payment_type
     
-    def initialize(args)
+    def initialize(args={})
       if args.is_a? Hash
         args[:allow_edit] ||= false 
         args[:payment_type] ||= CREDIT_CARD
@@ -14,6 +14,22 @@ module SallieMaeGateway
         }
       end
       @line_items ||= []
+    end
+
+    def school_id
+      @school_id || SALLIE_MAE_CONFIG['school_id']
+    end
+
+    def postback_url
+      @postback_url || SALLIE_MAE_CONFIG['postback_url']
+    end
+
+    def cancel_url
+      @cancel_url || SALLIE_MAE_CONFIG['cancel_url'] 
+    end
+
+    def finish_url
+      @finish_url || SALLIE_MAE_CONFIG['finish_url']
     end
     
     def total_amount
--- spec/models/sallie_mae_gateway/payment_spec.rb
+++ spec/models/sallie_mae_gateway/payment_spec.rb
@@ -1,45 +1,55 @@
 require 'spec_helper'
 
 describe SallieMaeGateway::Payment do
-  
-  subject do
-    SallieMaeGateway::Payment.new(
-      school_id: SALLIE_MAE_CONFIG["school_id"],
-      postback_url: SALLIE_MAE_CONFIG["postback_url"],
-      cancel_url: SALLIE_MAE_CONFIG["cancel_url"],
-      finish_url: SALLIE_MAE_CONFIG["finish_url"]
-    )
+ 
+  context "all values are provided" do 
+    subject do
+      SallieMaeGateway::Payment.new(
+        school_id: "1234",
+        postback_url: "http://localhost/postback",
+        cancel_url: "http://localhost/cancel", 
+        finish_url: "http://localhost/finish"
+      )
+    end
+    
+    let(:line_item_one) { SallieMaeGateway::LineItem.new(type: "HPRH", description: "Contract Prepayment", amount: 350.00) }
+    let(:line_item_two) { SallieMaeGateway::LineItem.new(type: "HPDP", description: "Dining Plan", amount: 170.00) }
+    
+    context "#initalization" do
+      its(:class) { should == SallieMaeGateway::Payment }
+      its(:school_id) { should == "1234" }
+      its(:postback_url) { should == "http://localhost/postback" }   
+      its(:cancel_url) { should == "http://localhost/cancel" }   
+      its(:finish_url) { should == "http://localhost/finish" } 
+      its(:line_items) { should be_a(Array) }
+      specify { subject.line_items.size.should == 0 }    
+    end
+    
+    context "#line_items" do
+      context "one line item is provided" do
+        before(:each) do 
+          subject.line_items << line_item_one
+        end
+        specify { subject.line_items.size.should == 1 }
+        its(:total_amount) { should == line_item_one.amount }
+      end
+      context "multiple line items are provided" do
+        before(:each) do
+          subject.line_items << line_item_one
+          subject.line_items << line_item_two
+        end
+        specify { subject.line_items.size.should == 2 }
+        its(:total_amount) { should == (line_item_one.amount + line_item_two.amount) }
+      end
+    end
   end
-  
-  let(:line_item_one) { SallieMaeGateway::LineItem.new(type: "HPRH", description: "Contract Prepayment", amount: 350.00) }
-  let(:line_item_two) { SallieMaeGateway::LineItem.new(type: "HPDP", description: "Dining Plan", amount: 170.00) }
-  
-  context "#initalization" do
-    its(:class) { should == SallieMaeGateway::Payment }
+
+  context "no values are provided" do
+    subject { SallieMaeGateway::Payment.new }
     its(:school_id) { should == SALLIE_MAE_CONFIG['school_id'] }
-    its(:postback_url) { should == SALLIE_MAE_CONFIG['postback_url'] }   
-    its(:cancel_url) { should == SALLIE_MAE_CONFIG['cancel_url'] }   
+    its(:postback_url) { should == SALLIE_MAE_CONFIG['postback_url'] }
+    its(:cancel_url) { should == SALLIE_MAE_CONFIG['cancel_url'] }
     its(:finish_url) { should == SALLIE_MAE_CONFIG['finish_url'] }
-    its(:line_items) { should be_a(Array) }
-    specify { subject.line_items.size.should == 0 }    
-  end
-  
-  context "#line_items" do
-    context "one line item is provided" do
-      before(:each) do 
-        subject.line_items << line_item_one
-      end
-      specify { subject.line_items.size.should == 1 }
-      its(:total_amount) { should == line_item_one.amount }
-    end
-    context "multiple line items are provided" do
-      before(:each) do
-        subject.line_items << line_item_one
-        subject.line_items << line_item_two
-      end
-      specify { subject.line_items.size.should == 2 }
-      its(:total_amount) { should == (line_item_one.amount + line_item_two.amount) }
-    end
   end
   
 end
